801fbd5a1751e5ecd8c44abb709d031b4e7de355
vscorza0093 <vinicius.scorza@hotmail.com>
2023-10-04 14:50:10 -0300

    can place flowers

--- exercises/can-place-flowers/Program.cs
+++ exercises/can-place-flowers/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] flowerbed = {1, 0, 0, 0, 1};
+int n = 2;
+
+
+for (int i = 0; i < flowerbed.Length; i++)
+{
+    if (flowerbed[i] == 0 && i > 0)
+    {
+        if (i == flowerbed.Length -1)
+        {
+            if (flowerbed[flowerbed.Length -1] == 0 && flowerbed[flowerbed.Length - 2] == 0)
+            {
+                flowerbed[flowerbed.Length -1] = 1;
+                n--;
+            }
+        }
+        else 
+        {
+            if (flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0)
+            {
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+    }
+    else if (flowerbed[i] == 0 && i == 0)
+    {
+        if (flowerbed[i + 1] == 0)
+        {
+            flowerbed[i] = 1;
+            n--;
+        }  
+    }
+    if (n == 0)
+        break;
+}
+
+foreach (var item in flowerbed)
+{
+    Console.WriteLine(item);
+}
+
+if (n > 0) 
+    Console.WriteLine(false);
+else
+    Console.WriteLine(true);

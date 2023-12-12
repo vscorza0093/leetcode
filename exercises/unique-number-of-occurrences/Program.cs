eb089d28cdc1fec107d7ea6da967c38eb914bf86
Vinicius Scorza <vinicius.scorza@hotmail.com>
2023-12-11 21:04:41 -0300

    Exercise resolution

---
+++ exercises/unique-number-of-occurrences/Program.cs
@@ -0,0 +1,41 @@
+﻿// Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+// Example 1:
+
+// Input: arr = [1,2,2,1,1,3]
+// Output: true
+// Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+// Example 2:
+
+// Input: arr = [1,2]
+// Output: false
+// Example 3:
+
+// Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+// Output: true
+
+Console.WriteLine(Solution.UniqueOccurrences([1,2,2,1,1,3]));
+
+public static class Solution {
+    public static bool UniqueOccurrences(int[] arr) {
+        Dictionary<int, int> keyValuePairs = new();
+        Dictionary<int, int> ocurrencesKeyValuePairs = new();
+
+        foreach (int number in arr)
+        {
+            if (keyValuePairs.ContainsKey(number))
+                keyValuePairs[number]++;
+            else
+                keyValuePairs.Add(number, 1);
+        }
+
+        foreach (int occurrences in keyValuePairs.Values)
+        {
+            if (ocurrencesKeyValuePairs.ContainsKey(occurrences))
+                return false;
+            else
+                ocurrencesKeyValuePairs.Add(occurrences, 1);
+        }
+
+        return true;
+    }
+}
---
+++ exercises/valid-anagram/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+
+*/
+
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+               Dictionary<char, int> keyValuePairs = new();
+
+        if (s.Length != t.Length)
+            return false;
+
+        foreach (char letter in s)
+        {
+            if (keyValuePairs.ContainsKey(letter))
+                keyValuePairs[letter]++;
+            else
+                keyValuePairs.Add(letter,1);
+        }
+
+        foreach (char letter in t)
+        {
+            if(keyValuePairs.ContainsKey(letter))
+                keyValuePairs[letter]--;
+            else
+                return false;
+            if(keyValuePairs[letter] < 0)
+                return false;
+        }
+        return true;       
+    }
+}
